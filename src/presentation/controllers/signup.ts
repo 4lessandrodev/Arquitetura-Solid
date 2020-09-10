6b324b30b3dd92850e7ef90f67ab3b1c6fce7a1b
Alessandro <alessandroadm@live.com>
2020-09-10 18:41:25 -0300

    feat: ensure SignUpController implements controller interface

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,10 @@
 import { IHttpRequest, IHttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
-export class SignUpController {
-  handler (httpRequest: IHttpRequest): IHttpResponse | any {
+import { Controller } from '../protocols/controller'
+
+export class SignUpController implements Controller {
+  handle (httpRequest: IHttpRequest): IHttpResponse | any {
     const requiredFields = ['email', 'name', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
