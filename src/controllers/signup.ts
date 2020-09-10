e562733672e8b072ee68ca01a320acf2cbe8b9b6
Alessandro <alessandroadm@live.com>
2020-09-10 16:17:07 -0300

    feat: ensure SignUpController returns an error if no name is no provider

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,5 +1,8 @@
 export class SignUpController {
   handler (httpRequest: any): any {
-    return { statusCode: 400 }
+    return {
+      statusCode: 400,
+      body: new Error('Faltando parâmetro: nome')
+    }
   }
 }
