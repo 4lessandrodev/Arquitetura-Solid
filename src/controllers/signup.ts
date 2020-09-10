1325c7b1f22697a27ebcf10b35c63dfb4a0a1533
Alessandro <alessandroadm@live.com>
2020-09-10 16:42:10 -0300

    feat: ensure SignUpController returns an error if no email is no provider

--- src/controllers/signup.ts
+++ src/controllers/signup.ts
@@ -1,8 +1,16 @@
 export class SignUpController {
   handler (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Faltando parâmetro: nome')
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Faltando parâmetro: nome')
+      }
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Faltando parâmetro: email')
+      }
     }
   }
 }
