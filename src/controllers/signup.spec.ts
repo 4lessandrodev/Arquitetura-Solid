41278aff909cb4952975b170fc0c46b9f59e47c2
Alessandro <alessandroadm@live.com>
2020-09-10 16:45:03 -0300

    test: ensure SignUpController returns an error if no email is no provider

--- src/controllers/signup.spec.ts
+++ src/controllers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Faltando parâmetro: nome'))
   })
+
+  test('Deve retornar 400 se o email não for informado', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handler(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Faltando parâmetro: email'))
+  })
 })
