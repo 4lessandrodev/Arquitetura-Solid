ede13580f1998f684dadaf02f513617c234c3976
Alessandro <alessandroadm@live.com>
2020-09-12 12:00:50 -0300

    test: esure SignUpController returns 400 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -86,6 +86,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new MissingParamError('passwordConfirmation'))
   })
 
+  test('Deve retornar 400 se a confirmação da senha falhar', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'invalid_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('passwordConfirmation'))
+  })
+
   test('Deve retornar 400 se for informado um email inválido', () => {
     const { sut, emailValidator } = makeSut()
     // jest.spyOn(emailValidator, 'isValid').mockReturnValue(false) // Duas formas de mockar o valor retornado
