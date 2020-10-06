f34b55d810b651b019799c970a4efdab1261af11
Alessandro <alessandroadm@live.com>
2020-10-06 19:06:23 -0300

    feat: ensure LogControllerDecorator LogErrorRepository if error

--- src/main/routes/decorators/log.ts
+++ src/main/routes/decorators/log.ts
@@ -1,15 +1,18 @@
+import { ILogErrorRepository } from '../../../data/protocols/Log-Error-Repository';
 import { IController, IHttpRequest, IHttpResponse } from '../../../presentation/protocols';
 export class LogControllerDecorator implements IController {
   private readonly controller: IController
+  private readonly logErrorRepository: ILogErrorRepository
 
-  constructor (controller: IController) {
+  constructor (controller: IController, logErrorRepository: ILogErrorRepository) {
     this.controller = controller;
+    this.logErrorRepository = logErrorRepository;
   }
 
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest);
     if (httpResponse.statusCode === 500) {
-      // log
+      await this.logErrorRepository.log(httpResponse.body.stack);
     }
     return httpResponse;
   }
