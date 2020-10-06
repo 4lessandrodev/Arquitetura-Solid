a83834b91eb6132c9b303abf5a77e3557b59571e
Alessandro <alessandroadm@live.com>
2020-10-06 17:13:30 -0300

    feat: ensure LogControllerDecorator return the same result of controller

--- src/main/routes/decorators/log.spec.ts
+++ src/main/routes/decorators/log.spec.ts
@@ -40,4 +40,23 @@ describe('LogController', () => {
     await sut.handle(httpRequest);
     expect(handleSpy).toHaveBeenCalledWith(httpRequest);
   });
+
+  test('Deve retornar o mesmo resultado do controller', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: 'any_mail@mail.com',
+        name: 'any_name',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(
+      {
+        statusCode: 200,
+        body: { name: 'Alessandro dev' }
+      }
+    );
+  });
 });
