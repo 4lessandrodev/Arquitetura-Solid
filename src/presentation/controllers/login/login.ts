e3b13011baf8322e992bba5a8670f4dd03a20435
Alessandro <alessandroadm@live.com>
2020-10-07 12:57:16 -0300

    feat: add call email validator on login

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -2,8 +2,14 @@ import { MissingParamError } from '../../errors';
 import { badRequest } from '../../helpers/http-helper';
 import { IHttpRequest, IHttpResponse } from '../../protocols';
 import { IController } from '../../protocols/controller';
+import { IEmailValidator } from '../signup/signup-protocols';
 
 export class LoginController implements IController {
+  private readonly emailValidator: IEmailValidator;
+  constructor (emailValidator: IEmailValidator) {
+    this.emailValidator = emailValidator;
+  }
+
   async handle (httpRequest: IHttpRequest): Promise<IHttpResponse> {
     if (!httpRequest.body.email) {
       return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))));
@@ -11,6 +17,7 @@ export class LoginController implements IController {
     if (!httpRequest.body.password) {
       return await new Promise(resolve => resolve(badRequest(new MissingParamError('password'))));
     }
+    this.emailValidator.isValid(httpRequest.body.email);
     return await new Promise(resolve => resolve(badRequest(new MissingParamError('email'))));
   }
 }
